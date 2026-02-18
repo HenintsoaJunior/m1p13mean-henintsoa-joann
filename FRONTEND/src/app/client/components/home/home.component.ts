9a01bee3beaa0bcebcf55c7cc54fa16735f35dbd
Daxido junior <143500070+HenintsoaJunior@users.noreply.github.com>
2026-02-18 19:46:32 +0300

    PUSH
    PUSH

--- FRONTEND/src/app/client/components/home/home.component.ts
+++ FRONTEND/src/app/client/components/home/home.component.ts
@@ -9,7 +9,7 @@ import { ToastService } from '../../../services/toast.service';
   standalone: true,
   imports: [CommonModule, NgIf],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   private authService = inject(AuthService);
@@ -32,7 +32,7 @@ export class HomeComponent {
     // Show "Devenir vendeur" button only if user exists and is not an admin or boutique
     return user ? user.role !== 'admin' && user.role !== 'boutique' : false;
   }
-  
+
   onProfileClick() {
     if (this.isLoggedIn) {
       this.showLogoutPopup = true;
@@ -40,18 +40,17 @@ export class HomeComponent {
       this.router.navigate(['/client-login']);
     }
   }
-  
+
   onLogout() {
     this.authService.logout();
     this.router.navigate(['/client-login']);
-    this.toastService.showInfo('Vous avez été déconnecté avec succès');
     this.showLogoutPopup = false;
   }
-  
+
   closePopup() {
     this.showLogoutPopup = false;
   }
-  
+
   getUserInitials(): string {
     const user = this.authService.getCurrentUser();
     if (user) {
@@ -61,7 +60,7 @@ export class HomeComponent {
     }
     return 'U';
   }
-  
+
   getUserEmail(): string {
     const user = this.authService.getCurrentUser();
     return user?.email || '';
