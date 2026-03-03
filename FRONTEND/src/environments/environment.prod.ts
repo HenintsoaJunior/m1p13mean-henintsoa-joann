90edec8eec71b32ccaef4503b77a82eec9943692
Daxido junior <143500070+HenintsoaJunior@users.noreply.github.com>
2026-03-03 19:55:14 +0300

    PUSH
    PUSH

--- FRONTEND/src/environments/environment.prod.ts
+++ FRONTEND/src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://m1p13mean-henintsoa-joann.vercel.app',
+  stripePublicKey:
+    'pk_test_51SiVuEHwaRuJ1EPle52pbufKNNyxKRzofipUQHzjhW4tVgUUhqZWR7P2JLqs3VTzjdH8S3IS5CxorrpLp97q73c300GI0S1Jta',
 };
